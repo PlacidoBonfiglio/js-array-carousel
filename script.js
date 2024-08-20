166cdfd9a48c56fb9d6c5e44cc6e324bac83b32f
PlacidoBonfiglio <bonfigliodino@gmail.com>
2024-08-20 16:53:21 +0200

    adding comments in js

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 console.log('JS OK');
 
+// Prendere elementi dal DOM
+// Aggiungo la classe active alla prima immagine
+// Rendere funzionanti i bottoni next e prev
+// Rimuovere la classe active all'immagine corrente ed aggiungerla alla successiva
+
 //Prendere elementi dal DOM
+const prevButton = document.getElementById('prev');
+const nextButton = document.getElementById('next');
+const images = document.querySelectorAll('#carousel img');
+
+// Aggiungo la classe active alla prima immagine
+let currentActiveIndex = 0
+images[currentActiveIndex].classList.add('active');
+
+// Aggiungere funziona al pulsante next
